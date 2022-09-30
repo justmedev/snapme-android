1a392e90eec5cae8a8c52ae2f3852a6143904694
ilja <ilja.ragnar@gmail.com>
2022-09-30 22:40:01 +0200

    added constructor without uuid and changed birthday from long to Date

--- app/src/main/java/dev/justme/snapme/helpers/Profile.java
+++ app/src/main/java/dev/justme/snapme/helpers/Profile.java
@@ -1,18 +1,29 @@
 package dev.justme.snapme.helpers;
 
+import java.util.Date;
+
 public class Profile {
-    private final String uuid;
+    private String uuid;
     private final String name;
-    private final long birthdayTimestamp;
+    private final Date birthday;
     private final String[] pictureUrls;
     private final int friends;
     private final String bio;
     private final String[] interests;
 
-    public Profile(String uuid, String name, String bio, long birthdayTimestamp, int friends, String[] pictureUrls, String[] interests) {
+    public Profile(String uuid, String name, String bio, Date birthday, int friends, String[] pictureUrls, String[] interests) {
         this.uuid = uuid;
         this.name = name;
-        this.birthdayTimestamp = birthdayTimestamp;
+        this.birthday = birthday;
+        this.pictureUrls = pictureUrls;
+        this.friends = friends;
+        this.bio = bio;
+        this.interests = interests;
+    }
+
+    public Profile(String name, String bio, Date birthday, int friends, String[] pictureUrls, String[] interests) {
+        this.name = name;
+        this.birthday = birthday;
         this.pictureUrls = pictureUrls;
         this.friends = friends;
         this.bio = bio;
@@ -27,8 +38,8 @@ public class Profile {
         return name;
     }
 
-    public long getBirthdayTimestamp() {
-        return birthdayTimestamp;
+    public Date getBirthday() {
+        return birthday;
     }
 
     public String[] getPictureUrls() {
